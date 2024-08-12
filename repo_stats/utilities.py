55a7c80e0249192dbbcfd3389efbf1144b01bbad
Jeff Jennings <jjennings@flatironinstitute.org>
2024-08-12 12:27:04 -0400

    utilities: add fill_missed_months

--- repo_stats/utilities.py
+++ repo_stats/utilities.py
@@ -28,3 +28,43 @@ def rolling_average(unaveraged, window):
     roll_avg = np.convolve(unaveraged, np.ones(window), mode="valid") / window
 
     return roll_avg, window
+
+
+def fill_missed_months(unique_output):
+    """
+    For an output of 'np.unique(x, return_counts=True)' where 'x' is a list of dates of the format '2024-01', fill in months missing in this list and set their count to 0.
+
+    Arguments
+    ---------
+    unique_output : tuple of array
+        Output of 'np.unique'
+
+    Returns
+    -------
+    unique_output : list of array
+        The input updated with inserted entries for missing months
+    """
+    unique_output = list(unique_output)
+
+    now = datetime.now(timezone.utc)
+
+    # build list of 'year-month' from oldest entry in 'unique_output' to current month
+    oldest, newest = min(unique_output[0]), f"{now.year}-{now.month:02d}"
+    years = [str(y) for y in list(range(int(oldest[:4]), int(newest[:4]) + 1))]
+    months = [f"{m:02d}" for m in list(range(1, 13))]
+    dates = []
+    for y in years:
+        for m in months:
+            dates.append(f"{y}-{m}")
+    dates = dates[dates.index(oldest) : dates.index(newest) + 1]
+
+    # insert missing dates into 'unique_output'
+    missed_months = [i for i in dates if i not in unique_output[0]]
+    for i in missed_months:
+        idx = np.searchsorted(unique_output[0], i)
+        unique_output[0] = np.insert(unique_output[0], idx, i)
+        unique_output[1] = np.insert(unique_output[1], idx, 0)
+
+    return unique_output
+
+
