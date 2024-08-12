34af427780b9be54adc1f017b7c84a20614ca440
Jeff Jennings <jjennings@flatironinstitute.org>
2024-08-12 12:38:56 -0400

    GitMetrics: add get_commits

--- repo_stats/git_metrics.py
+++ repo_stats/git_metrics.py
@@ -75,3 +75,150 @@ class GitMetrics:
 
         return parsed
 
+    def get_commits(self):
+        """
+        Obtain the commit history for a repository with 'git log', and parse the output.
+
+        Returns
+        -------
+        all_items : list of dict
+            A dictionary entry for each commit in the history, including the identifiers below in 'query'
+        """
+        print("\nCollecting git commit history")
+
+        cache_file = f"{self.cache_dir}/{self.repo_name}_commits.txt"
+        if not os.path.exists(cache_file):
+            open(cache_file, "w").close()
+
+        with open(cache_file, "r") as f:
+            old_items = f.readlines()
+            print(f"  {len(old_items)} commits found in cache at {cache_file}")
+        if old_items == []:
+            # NOTE: 'after' here differs from GitMetrics.get_issues_PRs, as does its type in 'query' below ('String' vs. 'String!') - see https://github.com/orgs/community/discussions/24443
+            after = None
+        else:
+            # convert string to dict and get relevant key
+            after = ast.literal_eval(old_items[-1].rstrip("\n"))["endCursor"]
+
+        # For query syntax, see https://docs.github.com/en/graphql/reference/objects#repository
+        # and https://docs.github.com/en/graphql/reference/objects#commit
+        # and https://docs.github.com/en/graphql/reference/objects#gitactor
+        # and https://docs.github.com/en/graphql/guides/using-pagination-in-the-graphql-api
+        # To quickly test a query, try https://docs.github.com/en/graphql/overview/explorer
+        query = """
+        query($owner: String!, $name: String!, $after: String) {
+            repository(name: $name, owner: $owner) {
+                ref(qualifiedName: "main") {
+                    target {
+                        ... on Commit {                    
+                            history(first: 100, after: $after) {
+                                pageInfo {
+                                    hasNextPage
+                                    endCursor
+                                }
+                        
+                                edges {
+                                    node {
+                                        oid
+                                        authoredDate
+                                        author {
+                                            name
+                                            email
+                                            user {
+                                                databaseId
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }                
+        """
+
+        headers = {"Authorization": f"token {self.token}"}
+        # with 'after', traverse through items (issues or PRs) from oldest to newest
+        variables = {
+            "owner": self.repo_owner,
+            "name": self.repo_name,
+            "after": after,
+        }
+        # must traverse through pages of items
+        hasNextPage = True
+
+        new_items = []
+        items_retrieved = 0
+        while hasNextPage is True:
+            response = requests.post(
+                "https://api.github.com/graphql",
+                json={"query": query, "variables": variables},
+                headers=headers,
+            )
+
+            if response.status_code == 200:
+                result = response.json()
+                try:
+                    result["data"]
+                except KeyError as err:
+                    print(f"Query syntax is likely wrong. Reponse to query: {result}")
+                    raise err
+
+                items_retrieved += len(
+                    result["data"]["repository"]["ref"]["target"]["history"]["edges"]
+                )
+
+                time_to_reset = datetime.fromtimestamp(
+                    int(response.headers["X-RateLimit-Reset"]) - time.time(),
+                    tz=timezone.utc,
+                ).strftime("%M:%S")
+
+                if (
+                    len(
+                        result["data"]["repository"]["ref"]["target"]["history"][
+                            "edges"
+                        ]
+                    )
+                    > 0
+                ):
+                    print(
+                        f"\r  Retrieved {items_retrieved} new commits (rate limit used: {response.headers['X-RateLimit-Used']} of {response.headers['X-RateLimit-Limit']} - resets in {time_to_reset})",
+                        end="",
+                        flush=True,
+                    )
+
+                    # store ID of the chronologically newest commit on current page, used to later reference newest item in cache
+                    result["data"]["repository"]["ref"]["target"]["history"]["edges"][
+                        -1
+                    ]["endCursor"] = result["data"]["repository"]["ref"]["target"][
+                        "history"
+                    ][
+                        "pageInfo"
+                    ][
+                        "endCursor"
+                    ]
+                    new_items.extend(
+                        result["data"]["repository"]["ref"]["target"]["history"][
+                            "edges"
+                        ]
+                    )
+
+                hasNextPage = result["data"]["repository"]["ref"]["target"]["history"][
+                    "pageInfo"
+                ]["hasNextPage"]
+
+                variables["after"] = result["data"]["repository"]["ref"]["target"][
+                    "history"
+                ]["pageInfo"]["endCursor"]
+
+            else:
+                raise Exception(f"Query failed -- return code {response.status_code}")
+
+        # prevent last flush, without printing new line
+        print("", end="")
+
+        all_items = update_cache(cache_file, old_items, new_items)
+
+        return all_items
+
